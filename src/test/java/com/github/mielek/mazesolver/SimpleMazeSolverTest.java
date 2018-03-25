d50b94e6b0bc6cf271ba85c218420d004a5547e9
Rafal Mielowski <mielowski.rafal@gmail.com>
2018-03-25 20:09:25 +0200

    TDD red. Added test for solving maze with one corridor with length of 3 in X axis

--- src/test/java/com/github/mielek/mazesolver/SimpleMazeSolverTest.java
+++ src/test/java/com/github/mielek/mazesolver/SimpleMazeSolverTest.java
@@ -2,43 +2,56 @@ package com.github.mielek.mazesolver;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-
 import static org.assertj.core.api.Assertions.*;
 
 public class SimpleMazeSolverTest {
     @Test
-    public void solveMazeWithOneLineCorridorInXAxis(){
-        int[][] board = new int[][]{{0},{0}};
+    public void solveMazeWithOneLineCorridorInXAxisWithTwoPoints() {
+        MazePoint dimension = MazePoint.of(2, 1);
+        int[][] board = new int[dimension.getX()][dimension.getY()];
         MazePoint start = MazePoint.of(0, 0);
         MazePoint target = MazePoint.of(1, 0);
         MazePoint[] expectedPath = new MazePoint[]{start, target};
 
-        solveMazeAndCheckExpectedPath(board,start,target,expectedPath);
+        solveMazeAndCheckExpectedPath(dimension, board, start, target, expectedPath);
     }
 
     @Test
-    public void startAndTargetAtTheSamePoint(){
-        int[][] board = new int[][]{{0},{0}};
-        MazePoint start = new MazePoint(0,0);
+    public void startAndTargetAtTheSamePoint() {
+        MazePoint dimension = MazePoint.of(1, 1);
+        int[][] board = new int[dimension.getX()][dimension.getY()];
+        MazePoint start = new MazePoint(0, 0);
         MazePoint target = start;
         MazePoint[] expectedPath = new MazePoint[]{start};
 
-        solveMazeAndCheckExpectedPath(board, start, target, expectedPath);
+        solveMazeAndCheckExpectedPath(dimension, board, start, target, expectedPath);
     }
 
     @Test
-    public void solveMazeWithOneLineCorridorInYAxis(){
-        int[][] board = new int[][]{{0, 0}};
-        MazePoint start = new MazePoint(0,0);
-        MazePoint target = new MazePoint(0,1);
+    public void solveMazeWithOneLineCorridorInYAxisWithTwoPoints() {
+        MazePoint dimension = MazePoint.of(1, 2);
+        int[][] board = new int[dimension.getX()][dimension.getY()];
+        MazePoint start = new MazePoint(0, 0);
+        MazePoint target = new MazePoint(0, 1);
         MazePoint[] expectedPath = new MazePoint[]{start, target};
 
-        solveMazeAndCheckExpectedPath(board, start, target, expectedPath);
+        solveMazeAndCheckExpectedPath(dimension, board, start, target, expectedPath);
     }
 
-    private void solveMazeAndCheckExpectedPath(int[][] board, MazePoint start, MazePoint target, MazePoint[] expectedPath) {
+    @Test
+    public void solveMazeWithOneLineCorridorInXAxisWithThreePoints() {
+        MazePoint dimension = MazePoint.of(3, 1);
+        int[][] board = new int[dimension.getX()][dimension.getY()];
+        MazePoint start = MazePoint.of(0, 0);
+        MazePoint target = MazePoint.of(2, 0);
+        MazePoint[] expectedPath = new MazePoint[]{start, MazePoint.of(1, 0), target};
+
+        solveMazeAndCheckExpectedPath(dimension, board, start, target, expectedPath);
+    }
+
+    private void solveMazeAndCheckExpectedPath(MazePoint dimension, int[][] board, MazePoint start, MazePoint target, MazePoint[] expectedPath) {
         Maze maze = Maze.builder()
+                .setDimension(dimension)
                 .setBoard(board)
                 .setStart(start)
                 .setTarget(target)
